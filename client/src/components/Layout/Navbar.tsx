2556f5e5662d860d8854ad9c7a6676cd61521170
Viktor Zetterström <viktor.zetterstrom@footway.com>
2021-10-30 13:42:29 +0200

    upgrade dependencies and add highlighting of current route

--- client/src/components/Layout/Navbar.tsx
+++ client/src/components/Layout/Navbar.tsx
@@ -1,26 +1,36 @@
-import styles from "./Navbar.module.css";
 import Link from "next/link";
+import { useRouter } from "next/dist/client/router";
+import clsx from "clsx";
 
-export const Navbar: React.FC = () => (
-  <nav className={styles.root}>
-    <div className={styles.left}>
-      <Link href="standings">
-        <a>standings</a>
-      </Link>
-      <Link href="games">
-        <a>games</a>
-      </Link>
-      <Link href="goalies">
-        <a>goalies</a>
-      </Link>
-      <Link href="skaters">
-        <a>skaters</a>
-      </Link>
-    </div>
-    <div className={styles.right}>
-      <Link href="https://zetterstrom.dev">
-        <a>zetterstrom.dev</a>
-      </Link>
-    </div>
-  </nav>
-);
+import styles from "./Navbar.module.css";
+
+export const Navbar: React.FC = () => {
+  const { pathname: currentPath } = useRouter();
+
+  const routes = ["standings", "games", "goalies", "skaters"];
+
+  return (
+    <nav className={styles.root}>
+      <div className={styles.left}>
+        {routes.map((route) => {
+          const routePath = `/${route}`;
+          const isActivePath = routePath === currentPath;
+
+          return (
+            <Link key={route} href={routePath}>
+              <a className={clsx(isActivePath ? styles.active : null)}>
+                {route}
+              </a>
+            </Link>
+          );
+        })}
+      </div>
+
+      <div className={styles.right}>
+        <Link href="https://zetterstrom.dev">
+          <a>zetterstrom.dev</a>
+        </Link>
+      </div>
+    </nav>
+  );
+};
