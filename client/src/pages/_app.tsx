d398e77ce62cfc10ad5212c1189172f98b776b67
Viktor Zetterström <viktor.zetterstrom@footway.com>
2021-11-12 16:47:23 +0100

    fix issues from lighthouse

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,10 +1,18 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import { Layout } from "../components";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div>
+      <Head>
+        <title>swedish hockey league 21/22</title>
+        <meta
+          name="description"
+          content="Swedish Hockey League Statistics"
+        ></meta>
+      </Head>
       <Layout>
         <Component {...pageProps} />
       </Layout>
